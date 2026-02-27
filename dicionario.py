4b6816673e3df5c041cae06f447fd53f1992296e
solar115 <zanettepedrohenrique@gmail.com>
2026-02-27 17:33:24 -0300

    Create dicionario.py

---
+++ dicionario.py
@@ -0,0 +1,13 @@
+meme_dict = {
+            "x1": "lutar contra a outra pessoa",
+            "peak": "algo bom",
+            }
+
+word = input("Digite uma palavra moderna que você não entende : ")
+
+if word in meme_dict.keys():
+   print(meme_dict[word])
+
+else:
+    print("a palavra não foi encontrada")
+  
